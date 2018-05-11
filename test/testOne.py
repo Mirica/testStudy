a2b40afc141edf8acff60a7dd1c2ef5060993523
Celia <merryshiyi@163.com>
2018-05-11 13:56:56 +0800

    练习题

--- test/testOne.py
+++ test/testOne.py
@@ -32,7 +32,7 @@ for idx in range(0,6):
     if a > arr[idx]:
         r += (a - arr[idx]) * rat[idx]
        # print((i - arr[idx]) * rat[idx])
-        #i = arr[idx]
+        #i = arr[idx] 
 print(r)
 
 
