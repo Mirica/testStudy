34ed0cfe320a312f3799b12e79798c0df272ae03
Celia <merryshiyi@163.com>
2018-05-11 12:12:07 +0800

    sth

--- test/testOne.py
+++ test/testOne.py
@@ -1,19 +1,6 @@
 #! /usr/bin/env python
 # coding : utf-8
-'''
-import requests
-url = 'http://japi.juhe.cn/qqevaluate/qq'
-par = {
-    'key ':'dddd',
-    'qq ':'123453565'
-    }
-r = requests.get(url,params = par)
-print(r.text)
-result = r.json()
 
-reason = result['reason']
-print(reason)
-'''
 #打印一颗圣诞树
 height = 5
 starts = 1
@@ -29,10 +16,3 @@ def b():
         b += 1
     bchange()
     print(b)
-
-li = [1,2,3,4,5] #从左往右取
-l1 = li[-3::1]#最后一个为负数，则从后往前
-print(l1)
-del li[0]
-li.append(8)
-print(li)
