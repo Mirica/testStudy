5131d93e20af16aef185146e8fec5f141e9508ca
Celia <merryshiyi@163.com>
2018-05-12 19:09:17 +0800

    数字类型

--- test/py002.py
+++ test/py002.py
@@ -9,18 +9,15 @@ e = 20 // 3  #取整
 f = 20 % 3
 g = 2 ** 5   #乘方
 print(a,b,c,d,e,f,g)
-#连接
-x = 'hello'
-y = 'world'
-z = 3
-t = 5
-print(x+y) #helloworld
-print((z+t)) # 8
-#切片
-s = '-hello world-'
-print(s[7:3:-1]) #w ol
-print(s.split('o')) #按照某个符号或字符，切割字符串，返回的是list['-hell', ' w', 'rld-']
-print(s.strip('-'))#去掉前后的某个符号，返回字符串hello world
-c = '[sjkhdla]'
-print(c)  #列表[sjkhdla]
-print(c.strip('[]'))#字符串sjkhdla
+#布尔类型  非0为真，0、空字符、none都是假
+a = None
+b = ''
+c = 0
+d = 'a'
+e = 23
+print(bool(a))
+print(bool(b))
+print(bool(c))
+print(bool(d))
+print(bool(c>e))  #运算 > < = !=  and or not
+print(bool(d and b))
